c6fa688df67c49ea9eda5c2e2709f475026e97e5
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-04-04 22:49:47 -0700

    Ran some npm scripts

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 
 const array = [1, 4, 2, 3, 1, 4, 5, 2, 3, 2, 1];
 const uniqueNums = lodash.uniq(array);
-console.log(chalk.bgGray(uniqueNums));
+console.log(chalk.green(array));
+console.log(chalk.yellow(uniqueNums));
 
 console.log(chalk.blueBright("Hello, with chalk!"));
